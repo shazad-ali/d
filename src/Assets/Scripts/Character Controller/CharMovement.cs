92882e8a12b3f12083ecc379205d900ed32e59d2
azure-dragon <shazadali09@hotmail.co.uk>
2013-08-15 18:07:58 +0100

    Prep Scene1
    smooth follow (camera)
    character movement

---
+++ src/Assets/Scripts/Character Controller/CharMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+		
+	public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+	
+	// Update is called once per frame
+	void Update () {
+		var controller = GetComponent<CharacterController>();
+	
+		//rotate around y - axis
+		transform.Rotate(0,Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		
+		var forward = transform.TransformDirection(Vector3.forward);
+		var curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);
+	}
+}
